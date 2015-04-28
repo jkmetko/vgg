6e9ecd4db918eb81ab47dd1ad7220c67974f42bd
jkmetko <j.kmetko@gmail.com>
2015-04-29 01:10:01 +0200

    tree structure OK

--- assets/js/IcicleGraph.js
+++ assets/js/IcicleGraph.js
@@ -1,66 +1,178 @@
 
-	var camera, controls, light, scene, renderer;
+var camera, controls, light, scene, renderer;
 
-	function icicle(path){
-		init();
-		animate();
-	}
-	
-	function init(){
-		/*CAMERA*/
-		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-		camera.position.z = 50;
-		
-		/*CONTROLS*/
-		controls = new THREE.TrackballControls( camera );
-		controls.addEventListener('change', render);
-		controls.rotateSpeed = 10;
-		controls.zoomSpeed = 12;
-		controls.panSpeed = 1;
-		controls.noZoom = false;
-		controls.noPan = false;
-		controls.staticMoving = true;
-		controls.dynamicDampingFactor = 0.3;
-		
-		scene = new THREE.Scene();
-		
-		/*LIGHTS*/
-		var ambientLight = new THREE.AmbientLight( 0x000000 );
-		scene.add( ambientLight );
-		
-		var lights = [];
-		lights[0] = new THREE.PointLight( 0xffffff, 1, 0 );
-		lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
-		lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
-		
-		lights[0].position.set( 0, 200, 0 );
-		lights[1].position.set( 100, 200, 100 );
-		lights[2].position.set( -100, -200, -100 );
+function icicle( path ){
+    checkFile( path )
+    init3D();
+    animate();
+}
 
-		scene.add( lights[0] );
-		scene.add( lights[1] );
-		scene.add( lights[2] );
-		
-		/*GEOMETRY*/
-		var geometry = new THREE.TorusKnotGeometry( 10, 3, 1000, 16 );
-		var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-		
-		var mesh = new THREE.Mesh( geometry, material );
-		scene.add(mesh);
-		
-		/*RENDERER*/
-		renderer = new THREE.WebGLRenderer();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(renderer.domElement);
-	}
-	
-	function animate(){
-		requestAnimationFrame( animate );
-		controls.update();
-		render();
-		
-	}
-	
-	function render(){
-		renderer.render( scene, camera );
-	}
+/*-------1.0 - PARSING GRAPH ML---------------------*/
+
+function parseGraphML( file ){
+    //GET NODES
+    var nodes = [];
+    $(file).find('node').each(function(){
+        nodes.push($(this).attr("id"));
+    })
+
+    //GET EDGES
+    graph = new Node(nodes[0]);
+    $(file).find('edge').each(function(){
+        addToGraph(graph, $(this));
+    })
+
+    console.log("NUMBER OF NODES: "+nodes.length);
+    console.log("NUMBER OF GRAPH NODES: "+numberOfGraphNodes);
+    drawGraph( graph );
+}
+
+var numberOfGraphNodes = 1;
+function addToGraph(graph, node){
+    source = node.attr("source");
+    target = node.attr("target");
+
+    if(graph != ""){
+        graph.findChild(source).addChild( new Node(target) );
+        numberOfGraphNodes++;
+    }
+
+    return graph;
+}
+
+function checkFile( path ) {
+    $.ajax({
+        type: "GET",
+        url: path,
+        dataType: "xml",
+        success:  function( output ){
+            parseGraphML( output )
+        },error: function(){
+            console.log("File '"+ path +"' does not exists")
+        }
+    })
+}
+
+/*--------2.0 - CREATING GRAPH----------------------*/
+function Node( id ){
+    this.id = id;
+    this.children = [];
+    this.parent = null;
+
+    this.setParentNode = function( node ){
+        this.parent = node;
+    }
+
+    this.getParentNode = function(){
+        return this.parent;
+    }
+
+    this.addChild = function( node ){
+        node.setParentNode(this);
+        this.children[this.children.length] = node;
+    }
+
+    this.getChildren = function(){
+        return this.children;
+    }
+
+    this.removeChildren = function(){
+        this.children = [];
+    }
+
+    this.findChild = function( id ){
+        if(this.id == id){
+            result = this;
+        }else if(this.getChildren() != ""){
+            children = this.getChildren();
+            children.forEach(function(child){
+                return child.findChild( id );
+            })
+        }
+        return result;
+    }
+
+}
+
+function findNode( nodes, node ){
+    nodes.forEach(function(rootNode){
+        if(rootNode.id == node.id){
+            return node;
+        }
+    })
+}
+
+function drawGraph( graph ){
+    console.log(graph.id);
+    $("#UI").append(graph.id+"<br />");
+    if(graph.getChildren() != ""){
+        children = graph.getChildren();
+        row = [];
+        children.forEach(function( node ){
+            row.push(node.id);
+            //drawGraph(node); //dorobit funkciu getRow
+        })
+        $("#UI").append(row+"<br />");
+    }
+}
+
+/*--------3.0 - CREATING 3D ENVIRONMENT-------------*/
+
+function init3D(){
+    /*CAMERA*/
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 50;
+
+    /*CONTROLS*/
+    controls = new THREE.TrackballControls( camera );
+    controls.addEventListener('change', render);
+    controls.rotateSpeed = 10;
+    controls.zoomSpeed = 12;
+    controls.panSpeed = 1;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+
+    scene = new THREE.Scene();
+
+    /*LIGHTS*/
+    var ambientLight = new THREE.AmbientLight( 0x000000 );
+    scene.add( ambientLight );
+
+    var lights = [];
+    lights[0] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+    lights[0].position.set( 0, 200, 0 );
+    lights[1].position.set( 100, 200, 100 );
+    lights[2].position.set( -100, -200, -100 );
+
+    scene.add( lights[0] );
+    scene.add( lights[1] );
+    scene.add( lights[2] );
+
+    /*GEOMETRY*/
+    var geometry = new THREE.TorusKnotGeometry( 10, 3, 1000, 16 );
+    var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add(mesh);
+
+    /*RENDERER*/
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function animate(){
+    requestAnimationFrame( animate );
+    controls.update();
+    render();
+
+}
+
+function render(){
+    renderer.render( scene, camera );
+}
--- assets/js/master.js
+++ assets/js/master.js
@@ -1,1 +1,2 @@
-icicle("")
+//CREATE ICICLE GRAPH
+icicle("data/graph1.graphml");
